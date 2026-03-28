cd798ff64cdcf83207b0a4a4334c4585bcd3b9b4
unknown <ayondevs.94b@gmail.com>
2026-03-28 17:47:33 +0530

    selected player components and removed funtionality

--- src/Components/HomePage/Players/Players.jsx
+++ src/Components/HomePage/Players/Players.jsx
@@ -13,17 +13,21 @@ const Players = ({ playersPromise, setCoin, coin }) => {
             <div className="flex justify-between items-center mb-5">
                 {
                     selectedType === 'available' ?
-                        <h2 className='font-bold text-2xl'>Available Players</h2>
-                        : <h2 className='font-bold text-2xl'> Selected Players</h2>
+                        (<h2 className='font-bold text-2xl'>Available Players</h2>)
+                        : (<h2 className='font-bold text-2xl'> Selected Players ({selectedPlayers.length}/{players.length})</h2>)
                 }
 
                 <div className="">
                     <button onClick={() => setSelectedType('available')} className={`btn ${selectedType === "available" ? 'bg-[#e7fe29]' : 'bg-white'}  rounded-r-none rounded-l-xl`}>Available</button>
-                    <button onClick={() => setSelectedType('selected')} className={`btn ${selectedType === "selected" ? 'bg-[#e7fe29]' : 'bg-white'}  rounded-l-none rounded-r-xl`}>Selected <span>(0)</span></button>
+                    <button onClick={() => setSelectedType('selected')} className={`btn ${selectedType === "selected" ? 'bg-[#e7fe29]' : 'bg-white'}  rounded-l-none rounded-r-xl`}>Selected({selectedPlayers.length})</button>
                 </div>
             </div>
 
-            {selectedType === 'available' ? <AvailablePlayers players={players} setCoin={setCoin} coin={coin} setSelectedPlayers={setSelectedPlayers} selectedPlayers={selectedPlayers} /> : <SelectedPlayers selectedPlayers={selectedPlayers}></SelectedPlayers>}
+            {selectedType === 'available' ?
+                <AvailablePlayers players={players} setCoin={setCoin} coin={coin}
+                    setSelectedPlayers={setSelectedPlayers} selectedPlayers={selectedPlayers} /> :
+
+                <SelectedPlayers selectedPlayers={selectedPlayers} setCoin={setCoin} coin={coin} setSelectedPlayers={setSelectedPlayers} ></SelectedPlayers>}
         </div>
     );
 };
--- src/Components/SelectedPlayers/SelectedPlayers.jsx
+++ src/Components/SelectedPlayers/SelectedPlayers.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { FaUser } from 'react-icons/fa';
+import { MdDelete } from 'react-icons/md';
 
-const SelectedPlayers = ({ selectedPlayers }) => {
+const SelectedPlayers = ({ selectedPlayers, setSelectedPlayers, coin, setCoin }) => {
     console.log('selectedPlayers', selectedPlayers);
+
+    const handleDeleteBtn = (player) => {
+        console.log('deleted', player);
+        const filteredPlayers = selectedPlayers.filter((selectedPlayer) => selectedPlayer.playerName != player.playerName )
+        setSelectedPlayers(filteredPlayers)
+        setCoin(coin + player.price)
+    }
+
     return (
-        <div>
-            selected players
+        <div className='space-y-5'>
+            {
+                selectedPlayers.map((player, i) => {
+                    return <div key={i} className="flex justify-between items-center p-10 rounded-2xl gap-6  bg-base-300">
+
+                        <div className="flex items-center gap-3">
+                            <img className='h-20 w-20 rounded shadow-sm border-none hover:shadow-xl border transition-shadow border-gray-500 duration-300' src={player.image} alt="" />
+                            <div className="">
+                                <h2 className="flex font-semibold items-center gap-2"><FaUser></FaUser> {player.playerName}</h2>
+                                <p className=" text-gray-400">{player.playerType}</p>
+                            </div>
+                        </div>
+                        <button className='btn text-red-500 text-2xl' onClick={() => handleDeleteBtn(player)}>
+                            <MdDelete />
+                        </button>
+
+                    </div>
+                })
+            }
         </div>
     );
 };
